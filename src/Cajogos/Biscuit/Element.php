83408dbf52347ae628807c095f0af7b5baa48937
Carlos Ferreira <c@rlos.rocks>
2017-07-29 13:11:46 +0100

    Added the Element class

---
+++ src/Cajogos/Biscuit/Element.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Cajogos\Biscuit;
+
+abstract class Element
+{
+	public static function get()
+	{
+		return new static();
+	}
+
+	/**
+	 * @throws Exception
+	 * @returns string
+	 */
+	public function getString()
+	{
+		throw new Exception('getString function not called in element: ' . get_class($this));
+	}
+}
