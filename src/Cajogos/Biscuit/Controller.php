0c61531478494a736a26dea3e615fa644b7ef6f3
Carlos Ferreira <c@rlos.rocks>
2017-07-29 01:12:43 +0100

    Namespace created

---
+++ src/Cajogos/Biscuit/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Cajogos\Biscuit;
+
+abstract class Controller
+{
+	public static function get()
+	{
+		return new static();
+	}
+
+	public static function display()
+	{
+		throw new Exception('No display function has been called from ' . get_class(static::get()));
+	}
+}
